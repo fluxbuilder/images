2140c19527bedd0db294b566889eecf556beca00
inspireuiteam <68225915+inspireuiteam@users.noreply.github.com>
2026-06-15 02:51:37 +0700

    Update 2026/6/0fe1110be2cf/1781466696162.js

---
+++ 2026/6/0fe1110be2cf/1781466696162.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'phone-cleaner-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/style.css',
+    '/script.js',
+    '/icon-192.png',
+    '/icon-512.png'
+];
+
+// تثبيت Service Worker
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+// جلب الملفات من الكاش أولاً
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
+});
+
+// تحديث الكاش
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
